8284bda43494738c1a5ab39ab651afa35e651b20
Zohaib619 <50499627+Zohaib619@users.noreply.github.com>
2023-05-11 09:06:34 -0700

    user list

---
+++ resources/views/user/list.blade.php
@@ -0,0 +1,56 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap demo</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+
+<body>
+    <h1>User Form</h1>
+
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="card">
+                    <div class="card-body">
+
+                        <table class="table">
+                            <thead>
+                                <tr>
+                                    <th>ID</th>
+                                    <th>Name</th>
+                                    <th>Email</th>
+                                    <th></th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td>1</td>
+                                    <td>ZOhiab</td>
+                                    <td>Zohaib@gmail.com</td>
+                                </tr>
+                                <tr>
+                                    <td>1</td>
+                                    <td>abc</td>
+                                    <td>abc@gmail.com</td>
+                                </tr>
+                                <tr>
+                                    <td>1</td>
+                                    <td>taha</td>
+                                    <td>taha@gmail.com</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js">
+    </script>
+</body>
+
+</html>
